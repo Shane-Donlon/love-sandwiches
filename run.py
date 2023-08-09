0c22777cc267c16bbb88d538153ca2216eb6b135
Shane-Donlon <shane.donlon@live.com>
2023-08-09 14:32:08 +0100

    Create function to get sales data from user input

--- run.py
+++ run.py
@@ -10,14 +10,21 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
 ]
-
+# const
 CREDS = Credentials.from_service_account_file("creds.json")
-
+# const
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-
+# const
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# const
 SHEET = GSPREAD_CLIENT.open("love-sandwiches")
 
-sales = SHEET.worksheet("sales")
-data = sales.get_all_values()
-print(data)
+def get_sales_data():
+    ''' Get sales figures from input'''
+    print("Please enter sales data from the last market")
+    print("Data should be six numbers, separated by a comma")
+    print("Example: 10,20,30,40,50,60 \n")
+    data_str = int(input("Please enter sales data"))
+    
+    
+get_sales_data()
